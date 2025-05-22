13d96c395c8f6b7c4f78164d94230aaa5280fb19
mnmnmnfffmm <164876062+mnmnmnfffmm@users.noreply.github.com>
2025-05-22 17:16:32 +0900

    feat: add new sentence and word pages

---
+++ src/pages/sentence.jsx
@@ -0,0 +1,14 @@
+import './sentence.css';
+
+function Sentence() {
+    return (
+        <div className="main">
+            <div className="gray-box"></div>
+            <div className="light-gray-box"></div>
+            <div className="input-box"></div>
+            <div className="gray-box"></div>
+        </div>
+    );
+}
+
+export default Sentence;
---
+++ src/pages/word.jsx
@@ -0,0 +1,14 @@
+import './word.css'
+
+function Word () {
+    return (
+        <div className='main'>
+            <div className='word_box'>
+
+            </div>
+            <div className='input_box' type='text' readOnly />
+        </div>
+    );
+}
+
+export default Word;
