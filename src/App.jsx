39ee8d372efc2743e0d21629e4496029e81eba9c
lnlp <chadg1212@naver.com>
2025-05-22 20:46:51 +0900

    feat :: 헤더 뷰 개발 및 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
 
+import Header from './components/Header/Header.jsx'
+
 function App() {
 
   return (
     <>
+      <Header />
     </>
   )
 }
---
+++ src/components/Header/Navigation.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './Navigation.module.css';
+import { useLocation } from 'react-router';
+
+const Navigation = () => {
+  const location = useLocation();
+
+  const navigation = [
+    { name: '홈', href: '/', current: false },
+    { name: '경쟁전', href: '/competition', current: false },
+    { name: '타자연습', href: '/practice', current: false },
+    { name: '커뮤니티', href: '/community', current: false },
+    { name: '문제집', href: '/problem', current: false },
+  ];
+  navigation.forEach(it=> {
+    if(it.href === location.pathname) {
+      it.current = true;
+    }
+  });
+
+  return (
+    <nav className={styles.nav}>
+      {navigation.map((item) => (
+        <a
+          key={item.name}
+          href={item.href}
+          className={`${styles.navLink} ${item.current ? styles.navLinkCurrent : styles.navLinkDefault}`}
+          aria-current={item.current ? 'page' : undefined}
+        >
+          {item.name}
+        </a>
+      ))}
+    </nav>
+  );
+};
+
+export default Navigation;
