d2fab08ce1a2d133e7355b0bd00afa3086eac375
haaeee <psh080706@naver.com>
2025-05-29 19:39:35 +0900

    feat: 문제 리스트 및 워크북 관련 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,7 @@ import LanguageSelection from './pages/LanguageSelection';
 const App = () => {
   return (
     <div>
-      <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
-        <Route path="/select-language" element={<LanguageSelection />} />
-      </Routes>
+      
     </div>
   );
 };
