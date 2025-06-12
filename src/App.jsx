254703a0db523c08851ad53cfb59e50907d73f52
lnlp <chadg1212@naver.com>
2025-06-12 20:44:36 +0900

    feat :: 풀코딩 기능 구현 및 Route 설정

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import { Routes, Route } from 'react-router';
 import './App.css'
 import Header from './components/Header/Header.jsx'
 import LanguageSelection from './pages/LanguageSelection'
+import Fullcode from './pages/Fullcode'
 import Home from './pages/Home'
+import WordPage from './pages/WordPage'
+import SentencePage from './pages/SentencePage'
+import TeacherView from './pages/TeacherView.jsx'
 
 const App = () => {
   return (
@@ -10,11 +14,14 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/SelectLanguage" element={<LanguageSelection />} />
+        <Route path="/full" element={<Fullcode />} />
+        <Route path="/SelectLanguage" element={<LanguageSelection />} /> 
+        <Route path="/word" element={<WordPage/>} />
+        <Route path="/sentence" element={<SentencePage/>} />
+        <Route path="/teacher" element={<TeacherView/>} />
       </Routes>
     </div>
   );
 };
 
 export default App;
-  
---
+++ src/pages/TeacherView.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TeacherView () {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default TeacherView;
