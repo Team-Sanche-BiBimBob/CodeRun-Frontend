70229375f091d685a2f39d9a038ae215afbe8e37
tjalsejr <minori5529@naver.com>
2025-06-16 09:02:30 +0900

    fix: auth style

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import "./Signup.css";
+import "./Auth.css";
 import { LoginTextField } from "../components/Auth/TextField/LoginTextField";
 
 const Signup = () => {
   return (
-    <div className="signup-wrapper">
-      <div className="signup-container">
-        <div className="signup-left">
+    <div className="auth-wrapper">
+      <div className="auth-container">
+        <div className="auth-left" style={{
+          backgroundImage: "url('/src/assets/authIcon/loginBanner.png')"
+        }}>
           <span className="logo-title">CodeRun{"{ }"}</span>
           <span className="sublogo-title">어제보다 한글자 더 빠르게</span>
         </div>
 
-        <div className="signup-right">
+        <div className="auth-right">
           <div className="input-group">
             <LoginTextField isLoginTextField={true} placeholder="이메일을 입력하세요" />
           </div>
@@ -21,7 +23,7 @@ const Signup = () => {
           <div className="input-group">
             <LoginTextField placeholder="비밀번호를 재입력하세요" />
           </div>
-          <button className="signup-btn">로그인</button>
+          <button className="auth-btn">회원가입</button>
         </div>
       </div>
     </div>
