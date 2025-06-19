92d6f94fdab00e28881b72e39695097004713594
haaeee <psh080706@naver.com>
2025-06-19 20:51:40 +0900

    문제집 페이지 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
-import { Routes, Route } from 'react-router';
+import { Routes, Route, useLocation } from 'react-router';
 import './App.css'
 import Header from './components/Header/Header.jsx'
 import LanguageSelection from './pages/LanguageSelection'
+import Fullcode from './pages/Fullcode'
 import Home from './pages/Home'
+import WordPage from './pages/WordPage'
+import SentencePage from './pages/SentencePage'
+import TeacherView from './pages/TeacherView.jsx'
+import CodingPlatform from './pages/CodingPlatform.jsx';
+import Fullcode from "./pages/Fullcode";
+
 
 const App = () => {
+  const location = useLocation();
+
+  const headerVisibleRoutes = ["/", "/select-language", "/competition"];
+
+  const showHeader = headerVisibleRoutes.includes(location.pathname);
+
   return (
-    <div>
-      <Header/>
+    <div style={{ paddingTop: '80px' }}>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/select-language" element={<LanguageSelection />} />
+        <Route path="/full" element={<Fullcode />} />
+        <Route path="/selectLanguage" element={<LanguageSelection />} />
+        <Route path="/word" element={<WordPage/>} />
+        <Route path="/sentence" element={<SentencePage/>} />
+        <Route path="/teacher" element={<TeacherView/>} />
+        <Route path="/platform" element={<CodingPlatform/>}/>
       </Routes>
     </div>
   );
 };
 
 export default App;
-
