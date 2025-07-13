ace6e1da6e2a41f7ffeb01d22ee16e180a7bc0fd
tjalsejr <minori5529@naver.com>
2025-07-13 19:17:24 +0900

    Login, SignUp view header delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
- 
 import { Routes, Route, useLocation } from 'react-router';
 import './App.css'
 import Header from './components/Header/Header.jsx'
@@ -14,26 +13,25 @@ import Signup from './pages/Signup.jsx';
 const App = () => {
   const location = useLocation();
 
-  const headerVisibleRoutes = ["/", "/select-language", "/competition"];
+  const headerVisibleRoutes = ["/", "/selectLanguage", "/full", "/word", "/sentence", "/teacher"];
 
   const showHeader = headerVisibleRoutes.includes(location.pathname);
 
   return (
-    <div style={{ paddingTop: '80px' }}>
-      <Header />
+    <div style={{ paddingTop: '0px' }}>
+      {showHeader && <Header />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/full" element={<Fullcode />} />
         <Route path="/selectLanguage" element={<LanguageSelection />} />
-        <Route path="/word" element={<WordPage/>} />
-        <Route path="/sentence" element={<SentencePage/>} />
-        <Route path="/teacher" element={<TeacherView/>} />
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/word" element={<WordPage />} />
+        <Route path="/sentence" element={<SentencePage />} />
+        <Route path="/teacher" element={<TeacherView />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
   );
 };
 
 export default App;
-
