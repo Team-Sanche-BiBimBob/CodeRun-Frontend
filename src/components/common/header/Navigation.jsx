76540dddb4a48de73a3aa260a314f40531879d55
lnlp <chadg1212@naver.com>
2025-09-17 23:27:16 +0900

    chore :: 헤더 라우팅 수정

--- src/components/common/header/Navigation.jsx
+++ src/components/common/header/Navigation.jsx
@@ -9,7 +9,7 @@ const Navigation = () => {
     { name: '홈', href: '/', current: false },
     { name: '타자연습', href: '/selectLanguage', current: false },
     { name: '문제집', href: '/problem', current: false },
-    { name: '아케이드', href: '/competition', current: false },
+    { name: '아케이드', href: '/arcadeSelect', current: false },
     { name: '학습방', href: '/study', current: false },
   ];
   navigation.forEach(it=> {
