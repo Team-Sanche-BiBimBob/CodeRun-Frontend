9db3b2db56867c630668968b5d6907f9518d37e4
lnlp <chadg1212@naver.com>
2025-09-17 17:56:04 +0900

    fix :: 버그 수정

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,17 @@ const App = () => {
     return () => window.removeEventListener('storage', onStorage);
   }, []); 
 
-  const headerVisibleRoutes = ["/", "/selectLanguage", "/full", "/word", "/sentence", "/teacher", "/problem", "/PracticeSelect" , "/competition"];
+  const headerVisibleRoutes = [
+    "/",
+    "/selectLanguage",
+    "/practiceSelect",
+    "/timeAttack",
+    "/word",
+    "/sentence",
+    "/full",
+    "/problem",
+    "/teacher"
+  ];
   const showHeader = headerVisibleRoutes.includes(location.pathname);
 
   return (
@@ -26,3 +36,4 @@ const App = () => {
 
 export default App;
 
+              
